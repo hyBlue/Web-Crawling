e3e94e9a2337cbf25a809a0706435f0ca2c1a137
gusdud1500 <gusdud1500@users.noreply.github.com>
2017-07-27 20:10:13 +0900

    naver webtoon image crawling

--- PR1/practice1.py
+++ PR1/practice1.py
@@ -2,6 +2,7 @@ import os
 import requests
 # URL 소스 : http://comic.naver.com/webtoon/detail.nhn?titleId=119874&no=1015&weekday=tue
 # - 덴마. 3-12화 1. 다이크(12)
+
 image_urls = [
 'http://imgcomic.naver.net/webtoon/119874/1015/20170528204207_6e9df8e618b97520233bbb35e7d4eaaf_IMAG01_1.jpg',
 'http://imgcomic.naver.net/webtoon/119874/1015/20170528204207_6e9df8e618b97520233bbb35e7d4eaaf_IMAG01_2.jpg',
